9de417fad9899988721ad8c42c5da87e9f9b8b82
Gabi Thaler <gmth054@gmail.com>
2025-08-04 12:09:58 +0300

    added count wprdes

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -7,16 +7,34 @@ class Analyzer:
         self.RD.Load_data()
         self.df=self.RD.Get_data()
         self.target_col = "Biased"
+        self.amounts ={}
+        self.new_df={}
+        self.avrg_word ={}
 
 
 
     def Count_biased(self):
         pass
         self.typas=self.df[self.target_col].value_counts()
-        self.amount_antisemi = self.typas[0]
-        self.amount_not_antisemi = self.typas[1]
+        for k in self.typas.index:
+            self.amounts[k]=self.typas[k]
+            mask = self.df[self.target_col] == k
+            filterd = self.df[mask].copy()
+            filterd.drop(self.target_col, axis=1, inplace=True)
+            self.new_df[k] = filterd
+
+
+    def count_worlds(self):
+        sum=0
+        for k in self.new_df.keys():
+            for v in self.new_df[k]["Text"]:
+                Text_arr=v.split()
+                sum=sum+len(Text_arr)
+            sum=sum/self.amounts[k]
+            self.avrg_word[k]=sum
 
 
 
 AA=Analyzer()
 AA.Count_biased()
+AA.count_worlds()
